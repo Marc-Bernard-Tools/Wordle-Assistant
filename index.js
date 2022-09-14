8973a5e4fe91db04cd3d12721ce1945e7fe3986f
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-09-14 12:34:01 -0400

    Add runtime

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import { ABAP } from "@abaplint/runtime";
 
-const AsyncFunction = new Function(`return Object.getPrototypeOf(async function(){}).constructor`)();
+async function myABAP() {
+    const AsyncFunction = new Function(`return Object.getPrototypeOf(async function(){}).constructor`)();
 
-const abap = new ABAP();
+    const abap = new ABAP();
 
-try {
-    abap.console.clear();
-    abap.statements.write(new abap.types.Character({ length: 5 }).set('hello'));
     try {
-        const f = new AsyncFunction("abapLocal", js);
-        await f(abap);
-        console.log(abap.console.get());
-    } catch (e) {
-        console.log("An error was thrown: " + e.toString());
+        abap.console.clear();
+        abap.statements.write(new abap.types.Character({ length: 5 }).set('hello'));
+        try {
+            const f = new AsyncFunction("abapLocal", js);
+            await f(abap);
+            console.log(abap.console.get());
+            document.getElementById("console").innerHTML = abap.console.get().toString();
+        } catch (e) {
+            console.log("An error was thrown: " + e.toString());
+        }
+    } catch (error) {
+        console.log(error.message);
     }
-} catch (error) {
-    console.log(error.message);
 }
