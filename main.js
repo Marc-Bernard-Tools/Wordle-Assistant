f5e79fb288e12e27f28a20d15d700e455c4c48bd
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-09-14 14:33:34 -0400

    Save

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-import { myABAP } from './modules/abap.js';
+const ABAP = require('@abaplint/runtime');
 
-function callABAP() {
+function myABAP() {
+    try {
+        const abap = new ABAP();
+
+        abap.console.clear();
+        abap.statements.write(new abap.types.Character({ length: 5 }).set('hello'));
+        try {
+            abap();
+        } catch (e) {
+            console.log("An error was thrown: " + e.toString());
+        }
+        const output = abap.console.get();
+        console.log(output);
+        return output.toString();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function runABAP() {
     document.getElementById("console").innerHTML = myABAP();
 }
