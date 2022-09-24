3bfbeb68e750f59ce3b6d21faa610cbf7af68386
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-09-24 13:01:08 -0400

    Save

---
+++ abap.js
@@ -0,0 +1,26 @@
+import { ABAP } from '@abaplint/runtime';
+
+export function myABAP() {
+    try {
+        const abap = new ABAP();
+        abap.console.clear();
+
+        // >>> BEGIN of transpiled ABAP code >>>
+
+        abap.statements.write(new abap.types.Character({ length: 5 }).set('hello'));
+        
+        // <<< END of transpiled ABAP code <<<
+        
+        try {
+            abap();
+        } catch (e) {
+            console.log("An error was thrown: " + e.toString());
+        }
+
+        const output = abap.console.get();
+        console.log(output);
+        return output.toString();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-require('@abaplint/runtime');
-
-function myABAP() {
-    try {
-        const abap = new ABAP();
-        abap.console.clear();
-
-        // >>> BEGIN of transpiled ABAP code >>>
-
-        abap.statements.write(new abap.types.Character({ length: 5 }).set('hello'));
-        
-        // <<< END of transpiled ABAP code <<<
-        
-        try {
-            abap();
-        } catch (e) {
-            console.log("An error was thrown: " + e.toString());
-        }
-
-        const output = abap.console.get();
-        console.log(output);
-        return output.toString();
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+require('./abap.js');
 
 global.runABAP = function() {
     document.getElementById("console").innerHTML = myABAP();
