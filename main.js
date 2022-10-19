d72425124d9fa9e871f42d7693b041893be492c2
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-10-19 00:49:34 -0400

    Fix Uncaught TypeError: run is not a function

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const abaplint = require("./node_modules/@abaplint/runtime/build/src/index");
 
 const abap = new abaplint.ABAP();
 
-const AsyncFunction = new Function(`return Object.getPrototypeOf(async function(){}).constructor`)();
+// const AsyncFunction = new Function(`return Object.getPrototypeOf(async function(){}).constructor`)();
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AsyncFunction
+const AsyncFunction = (async function () { }).constructor;
 
 async function runABAP(letter1, letter2, letter3, letter4, letter5, black, orange) {
     try {
@@ -48,7 +50,18 @@ async function runABAP(letter1, letter2, letter3, letter4, letter5, black, orang
     }
 }
 
-global.run = async function run() {
+//global.run = async function run() {
+global.run = function run() {
+    const asyncRunABAP = new AsyncFunction(
+        'letter1',
+        'letter2',
+        'letter3',
+        'letter4',
+        'letter5',
+        'black',
+        'orange',
+        'return await runABAP(letter1, letter2, letter3, letter4, letter5, black, orange);',
+    );
     const app = document.getElementById("app");
     const letter1 = document.getElementById("letter1").value;
     const letter2 = document.getElementById("letter2").value;
@@ -57,5 +70,5 @@ global.run = async function run() {
     const letter5 = document.getElementById("letter5").value;
     const black = document.getElementById("black").value;
     const orange = document.getElementById("orange").value;
-    app.textContent = await runABAP(letter1, letter2, letter3, letter4, letter5, black, orange);
+    app.textContent = asyncRunABAP(letter1, letter2, letter3, letter4, letter5, black, orange);
 }
